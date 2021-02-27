981ead0b9f9de289e82fafe764d1606d3285f61c
Roberto-Parra <w.parrav@uniandes.edu.co>
2021-02-27 20:10:19 -0300

    Caso 4: Calcular promedio con n elementos (Rojo)

--- tests/test_conjunto.py
+++ tests/test_conjunto.py
@@ -10,6 +10,9 @@ class TestConjunto(unittest.TestCase):
     def test_conjunto_un_elemento(self):
         conjunto = Conjunto([5])
         self.assertEqual(conjunto.promedio(),5)
-    def test_conjunto_dos_elemento(self):
+    def test_conjunto_dos_elementos(self):
         conjunto = Conjunto([5,7])
-        self.assertEqual(conjunto.promedio(),6)               
+        self.assertEqual(conjunto.promedio(),6)
+    def test_conjunto_n_elementos(self):
+        conjunto = Conjunto([2,3,4,5,6])
+        self.assertEqual(conjunto.promedio(),(2+3+4+5+6)/5)                   
